d8b430a2b56b20d365c7f36d2b099c44f609bf9a
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-04-15 01:50:54 -0500

    iai benchmarks included

---
+++ temple_parse/benches/criterion.rs
@@ -0,0 +1,35 @@
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use std::borrow::Borrow;
+use std::fs::{File, OpenOptions};
+use std::path::PathBuf;
+use temple_parse::{Contents, Indicator, Keys, Parse};
+
+
+pub fn replace_and_write(c: &mut Criterion) {
+    c.bench_function("String conversion to keys and write of single file", |b| {
+        b.iter(|| {
+            let mut contents = Contents::from(black_box("lmao {{ jaja }}"));
+            let start = black_box(Indicator::from("{{ ", true)).unwrap();
+            let end = black_box(Indicator::from(" }}", false)).unwrap();
+            let keys = Keys::from_string("jaja=perro,");
+            let replace = contents.replace(start, end, &keys);
+
+            let r = if let Ok(res) = replace {
+                match res.0 {
+                    666 => "No changes. No keys".to_string(),
+                    _ => Contents::get_str_from_result(&res.1).to_string(),
+                }
+            } else {
+                "Invalid chars or data".to_string()
+            };
+
+            println!("{r}");
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    replace_and_write
+);
+criterion_main!(benches);
