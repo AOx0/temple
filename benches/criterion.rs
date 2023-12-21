f2adf489982e72008a7b3f95a6551ac64271c9a3
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-12-21 07:31:54 -0600

    Rm benches

--- benches/criterion.rs
+++
@@ -1,8 +0,0 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-
-pub fn replace_and_write(c: &mut Criterion) {
-    c.bench_function("String conversion to keys and write of single file", |_| {});
-}
-
-criterion_group!(benches, replace_and_write);
-criterion_main!(benches);
